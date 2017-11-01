c3ef8546d8fb215ab2d54a549c74f33600b89bbe
Ethan Neff <ethan.neff@eneff.com>
2017-10-31 18:17:23 -0700

    test: sanity check

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,7 +7,7 @@ const page = props => {
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Changes you make will automatically reload.</Text>
       <Text>Shake your phone to open the developer menu.</Text>
-      <Text>Test</Text>
+      <Text>Test test</Text>
     </View>
   );
 };
