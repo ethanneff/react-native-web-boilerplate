f4581b82e3668e2467a8627bfc462abac9de6d3b
Ethan Neff <ethan.neff@eneff.com>
2017-11-04 09:08:02 -0700

    fix: added theme (need to refactor and move to window)

---
+++ src/common/theme/index.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  window: {
+    marginVertical: 0,
+    marginHorizontal: "auto",
+    width: "100%",
+    maxWidth: 800
+  }
+});
