13858b5f611c240ab0d817cf8620843074a65063
Ethan Neff <ethan.neff@eneff.com>
2018-03-09 16:53:29 -0800

    style: index cleanup

--- src/index.js
+++ src/index.js
@@ -1,38 +1,36 @@
 // entry
 import React from "react";
 import { Provider } from "react-redux";
-import { AppRegistry, Platform, Button } from "react-native";
+import { AppRegistry, Platform } from "react-native";
 import { Router, AndroidBackButton } from "./common/routing";
-import registerServiceWorker from "./common/web/registerServiceWorker";
 import { store } from "./common/store";
+import registerServiceWorker from "./common/web/registerServiceWorker";
 import App from "./containers/app";
 import Navigation from "./containers/navigation";
 import Counter from "./containers/counter";
 
-import { ConnectedRouter, push } from "react-router-redux";
+const APP_NAME = "rnb";
 
 // TODO: handle navigation with redux
 // TODO: remove Navigation and Counter (will be handled by redux router)
-const main = props => {
-  return (
-    <Provider store={store}>
-      <Router>
-        <AndroidBackButton>
-          <App statusBar={false}>
-            <Navigation />
-            <Counter title="count" />
-          </App>
-        </AndroidBackButton>
-      </Router>
-    </Provider>
-  );
-};
+const main = () => (
+  <Provider store={store}>
+    <Router>
+      <AndroidBackButton>
+        <App statusBar={false}>
+          <Navigation />
+          <Counter title="count" />
+        </App>
+      </AndroidBackButton>
+    </Router>
+  </Provider>
+);
 
 export default main;
 
+AppRegistry.registerComponent(APP_NAME, () => main);
 if (Platform.OS === "web") {
-  AppRegistry.registerComponent("rnb", () => main);
-  AppRegistry.runApplication("rnb", {
+  AppRegistry.runApplication(APP_NAME, {
     rootTag: document.getElementById("root")
   });
   registerServiceWorker();
